65949b1e0044a7af437111faefd7ce8fd0bee01f
Taner Özer <tanerozer16@gmail.com>
2026-09-15 11:31:58 +0300

    db edited

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);

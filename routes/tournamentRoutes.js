e68f8a9f07cfafccd00d2960854784321223a517
Taner Özer <tanerozer16@gmail.com>
2026-09-18 13:24:36 +0300

    Add knockout stage routes to tournament API

--- routes/tournamentRoutes.js
+++ routes/tournamentRoutes.js
@@ -7,5 +7,23 @@ router.get("/league", tournamentController.getLeagueTable);
 router.get("/knockout", tournamentController.getKnockoutPage);
 router.post("/start-season", tournamentController.startNewSeason);
 router.post("/simulate-all", tournamentController.simulateAllMatches);
+router.post(
+  "/knockout/generate-playoffs",
+  tournamentController.generatePlayoffs,
+);
+router.post(
+  "/knockout/generate-round-of-16",
+  tournamentController.generateRoundOf16,
+);
+router.post(
+  "/knockout/generate-quarter",
+  tournamentController.generateQuarterFinals,
+);
+router.post("/knockout/generate-semi", tournamentController.generateSemiFinals);
+router.post("/knockout/generate-final", tournamentController.generateFinal);
+router.post(
+  "/knockout/simulate-stage",
+  tournamentController.simulateKnockoutStage,
+);
 
 module.exports = router;

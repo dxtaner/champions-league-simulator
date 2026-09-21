2a9476b011935330398ab8838e42928ce20e6306
Taner Özer <tanerozer16@gmail.com>
2026-09-21 15:28:15 +0300

    Enhance knockout match simulation with penalty handling and aggregate scoring logic

--- utils/knockoutSimulator.js
+++ utils/knockoutSimulator.js
@@ -42,7 +42,23 @@ exports.simulateKnockoutMatch = async (matchId) => {
   let homeScore = generateGoals(Math.max(0.4, (homePower / awayPower) * 1.2));
   let awayScore = generateGoals(Math.max(0.3, (awayPower / homePower) * 1.0));
 
-  if (match.leg === 2) {
+  if (match.stage === "FINAL") {
+    if (homeScore === awayScore) {
+      if (Math.random() > 0.5) homeScore += 1;
+      if (Math.random() > 0.5) awayScore += 1;
+
+      if (homeScore === awayScore) {
+        const { homePen, awayPen } = simulatePenalties();
+        match.penaltyHome = homePen;
+        match.penaltyAway = awayPen;
+        match.winner = homePen > awayPen ? homeTeam._id : awayTeam._id;
+      }
+    }
+
+    if (!match.winner) {
+      match.winner = homeScore > awayScore ? homeTeam._id : awayTeam._id;
+    }
+  } else if (match.leg === 2) {
     const firstLeg = await Match.findOne({
       stage: match.stage,
       leg: 1,
@@ -53,24 +69,26 @@ exports.simulateKnockoutMatch = async (matchId) => {
     });
 
     if (firstLeg) {
-      const isHomeFirst =
-        firstLeg.homeTeam.toString() === match.homeTeam.toString();
-      const firstLegHomeGoals = isHomeFirst
-        ? firstLeg.homeScore
-        : firstLeg.awayScore;
-      const firstLegAwayGoals = isHomeFirst
-        ? firstLeg.awayScore
-        : firstLeg.homeScore;
+      let currentHomeLeg1Goals = 0;
+      let currentAwayLeg1Goals = 0;
 
-      let aggHome = homeScore + firstLegAwayGoals;
-      let aggAway = awayScore + firstLegHomeGoals;
+      if (firstLeg.homeTeam.toString() === match.homeTeam.toString()) {
+        currentHomeLeg1Goals = firstLeg.homeScore;
+        currentAwayLeg1Goals = firstLeg.awayScore;
+      } else {
+        currentHomeLeg1Goals = firstLeg.awayScore;
+        currentAwayLeg1Goals = firstLeg.homeScore;
+      }
+
+      let aggHome = homeScore + currentHomeLeg1Goals;
+      let aggAway = awayScore + currentAwayLeg1Goals;
 
       if (aggHome === aggAway) {
         if (Math.random() > 0.5) homeScore += 1;
         if (Math.random() > 0.5) awayScore += 1;
 
-        aggHome = homeScore + firstLegAwayGoals;
-        aggAway = awayScore + firstLegHomeGoals;
+        aggHome = homeScore + currentHomeLeg1Goals;
+        aggAway = awayScore + currentAwayLeg1Goals;
 
         if (aggHome === aggAway) {
           const { homePen, awayPen } = simulatePenalties();
@@ -83,26 +101,15 @@ exports.simulateKnockoutMatch = async (matchId) => {
       if (!match.winner) {
         match.winner = aggHome > aggAway ? homeTeam._id : awayTeam._id;
       }
-    }
-  } else if (match.stage === "FINAL") {
-    if (homeScore === awayScore) {
-      if (Math.random() > 0.5) homeScore += 1;
-      else awayScore += 1;
 
-      if (homeScore === awayScore) {
-        const { homePen, awayPen } = simulatePenalties();
-        match.penaltyHome = homePen;
-        match.penaltyAway = awayPen;
-        match.winner = homePen > awayPen ? homeTeam._id : awayTeam._id;
-      }
-    }
-    if (!match.winner) {
-      match.winner = homeScore > awayScore ? homeTeam._id : awayTeam._id;
+      firstLeg.winner = match.winner;
+      await firstLeg.save();
     }
   }
 
   match.homeScore = homeScore;
   match.awayScore = awayScore;
   match.played = true;
+
   await match.save();
 };
